d5e42f02bad00b929ee156a35ca015d12c9bc264
maxcodingtx <maxcodingtx@gmail.com>
2025-06-06 14:24:23 -0500

    define Button component

---
+++ src/common/Button.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface ButtonProps {
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  children: React.ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = ({ onClick, children }) => {
+  const buttonStyles =
+    "mx-[10px] my-[8px] cursor-pointer rounded-[6px] border-[4px] border-solid border-sky-300 bg-white px-[12px] py-[9px] font-sans font-[550] text-black shadow-[0px_4px_6px_0px_rgba(0,_0,_0,_0.1)] hover:bg-gray-200 active:scale-95";
+
+  return (
+    <button className={buttonStyles} onClick={onClick} type="button">
+      {children}
+    </button>
+  );
+};
+
+export default Button;
