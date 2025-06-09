6620546e0cda9b2377406b6532be75bd681f8ef0
maxcodingtx <maxcodingtx@gmail.com>
2025-06-08 22:36:10 -0500

    add margins to Input component

--- src/common/Input.tsx
+++ src/common/Input.tsx
@@ -24,7 +24,7 @@ const Input: React.FC<InputProps> = ({
   disabled = false,
 }) => {
   return (
-    <div>
+    <div className="m-[8px]">
       <label
         htmlFor={id}
         className="mb-0 block text-sm font-bold text-gray-700"
