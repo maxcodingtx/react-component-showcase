15251f0d1a8610643605fa6bf786affa933e1904
maxcodingtx <maxcodingtx@gmail.com>
2025-06-18 14:26:47 -0500

    create NavbarApp.tsx file.

---
+++ src/components/React-Navbar/NavbarApp.tsx
@@ -0,0 +1,35 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import NavigationBar from "./navbar";
+import AboutPage from "./pages/about";
+import ContactPage from ".//pages/contact";
+import HomePage from "./pages/home";
+
+function App() {
+  return (
+    <body className="bg-gradient-to-r from-indigo-400 to-cyan-400">
+      <BrowserRouter>
+        <NavigationBar WebsiteName="Test Name" />
+        <Routes>
+          <Route path="/pages/about" element={<AboutPage />} />
+          <Route
+            path="/pages/contact"
+            element={
+              <ContactPage
+                ContactMethodInfo={[
+                  "000-000-0000",
+                  "example@gmail.com",
+                  "123 abc Street",
+                  "@websiteSOCIAL",
+                ]}
+                ContactMethods={["Phone", "Email", "Address", "Twitter"]}
+              />
+            }
+          />
+          <Route path="/pages/home" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </body>
+  );
+}
+
+export default App;
