3130028c6e82c07d0ca52092b1977e69746a2ae2
maxcodingtx <maxcodingtx@gmail.com>
2025-06-21 00:43:18 -0500

    create header containing logo and links.

---
+++ src/web-apps/MacBook-app/components/Header.tsx
@@ -0,0 +1,38 @@
+import type { JSX } from "react";
+import AppleLogo from "../images/apple-logo.png";
+
+const Logo = (): JSX.Element => {
+  return (
+    <div className="">
+      <img className="w-10" src={AppleLogo} alt="Apple Logo" />
+    </div>
+  );
+};
+
+const NavLinks = (): JSX.Element => {
+  return (
+    <div className="flex">
+      <a className="mr-5 text-lg font-semibold" href="#">
+        Overview
+      </a>
+      <a className="mr-5 text-lg font-semibold" href="#">
+        Pricing
+      </a>
+      <a className="text-lg font-semibold" href="#">
+        Features
+      </a>
+    </div>
+  );
+};
+
+const NavBar = (): JSX.Element => {
+  return (
+    <div className="my-3 flex items-center justify-between">
+      <Logo />
+
+      <NavLinks />
+    </div>
+  );
+};
+
+export default NavBar;
