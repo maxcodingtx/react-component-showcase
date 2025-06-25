35cc1d977b325114805b33473af4ea3dcd62a5f8
maxcodingtx <maxcodingtx@gmail.com>
2025-06-25 00:44:38 -0500

    create barrel.ts file

---
+++ src/web-apps/MacBook-app/components/barrel.ts
@@ -0,0 +1,5 @@
+export { NavBar } from "./Header";
+export { HeroSection } from "./HeroSection";
+export { OverviewSection } from "./OverviewSection";
+export { PricingSection } from "./PricingSection";
+export { TestimonialSection } from "./TestimonialSection";
