7186e5cab839b362f590bd6f4ae62e1bb9f29dbf
maxcodingtx <maxcodingtx@gmail.com>
2025-07-01 12:28:23 -0500

    apply styles for larger screens.

--- src/web-apps/Business-app/src/components/Footer.tsx
+++ src/web-apps/Business-app/src/components/Footer.tsx
@@ -3,49 +3,58 @@ import React from "react";
 const Footer: React.FC = () => {
   return (
     <footer className="bg-[#ece4a9] *:text-[#053052]">
-      <h1 className="px-3 py-5 font-[Avenir] text-3xl font-bold">Foo</h1>
-      <hr />
-      <div className="px-3 py-3 font-[Quicksand]">
-        <h4 className="py-3 text-xl font-bold">Your Creative Partner</h4>
-        <p>Email *</p>
-        <input className="my-3 w-full border-b border-b-black" type="text" />
-        <button
-          className="w-full border border-black py-2 hover:bg-[#053052] hover:text-white"
-          type="button"
-        >
-          Submit
-        </button>
-      </div>
+      <h1 className="px-3 py-5 font-[Avenir] text-3xl font-bold md:text-4xl">
+        Foo
+      </h1>
+      <hr className="md:hidden" />
 
-      <div className="mt-5 border-y px-3 py-3 font-[Quicksand] *:mb-5">
-        {/* contact and location info */}
-        <div className="font-light">
-          <p>123-456-7890</p>
-          <p>info@mysite.com</p>
-        </div>
-        <div className="font-light">
-          <p>1234 Street Name</p>
-          <p>San Francisco, CA 94199</p>
+      <div className="*:p-3 md:grid md:grid-cols-4 md:text-2xl md:*:flex md:*:flex-col md:*:justify-center md:*:px-6 md:*:py-12">
+        <div className="col-span-2 font-[Quicksand] md:border-y">
+          <h4 className="py-3 text-xl font-bold">Your Creative Partner</h4>
+          <p>Email *</p>
+          <div className="max-w-1/2">
+            <input
+              className="my-3 w-full border-b border-b-black"
+              type="text"
+            />
+            <button
+              className="w-full border border-black py-2 hover:bg-[#053052] hover:text-white"
+              type="button"
+            >
+              Submit
+            </button>
+          </div>
         </div>
 
-        {/* social bar */}
-        <div className="text-sm">
-          <ul className="flex gap-3 *:text-[#053052] *:underline">
-            <li>
-              <a href="#">Facebook</a>
-            </li>
-            <li>
-              <a href="#">Twitter</a>
-            </li>
-            <li>
-              <a href="#">Instagram</a>
-            </li>
-          </ul>
-        </div>
+        <div className="border-y font-[Quicksand] md:border *:md:text-center">
+          {/* contact and location info */}
+          <div className="">
+            <div className="font-light">
+              <p>123-456-7890</p>
+              <p>info@mysite.com</p>
+              <p>1234 Street Name</p>
+              <p>San Francisco, CA 94199</p>
+            </div>
+          </div>
 
+          {/* social bar */}
+          <div className="">
+            <ul className="flex gap-3 *:text-center *:text-[#053052] md:justify-center">
+              <li>
+                <a href="#">Facebook</a>
+              </li>
+              <li>
+                <a href="#">Twitter</a>
+              </li>
+              <li>
+                <a href="#">Instagram</a>
+              </li>
+            </ul>
+          </div>
+        </div>
         {/* external links */}
-        <div className="text-sm">
-          <ul className="flex flex-col *:underline">
+        <div className="font-[Quicksand] md:border-y md:border-r">
+          <ul className="flex flex-col *:underline md:text-center">
             <li>
               <a href="#">Terms of Service</a>
             </li>
@@ -60,7 +69,7 @@ const Footer: React.FC = () => {
       </div>
 
       {/* copyright */}
-      <p className="px-3 py-3">© 2023 Foo, All rights reserved.</p>
+      <p className="p-3 md:text-2xl">© 2023 Foo, All rights reserved.</p>
     </footer>
   );
 };
