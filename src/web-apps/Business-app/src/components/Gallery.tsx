4cf877902356fff47485855c41a3deffe7af9bf8
maxcodingtx <maxcodingtx@gmail.com>
2025-07-01 11:19:15 -0500

    Add key for images, import React and get rid of console.log statement.

--- src/web-apps/Business-app/src/components/Gallery.tsx
+++ src/web-apps/Business-app/src/components/Gallery.tsx
@@ -1,4 +1,4 @@
-// import React from "react";
+import React from "react";
 
 const images = Object.values(
   import.meta.glob("../images/gallery/*.png", {
@@ -7,8 +7,6 @@ const images = Object.values(
     import: "default",
   }),
 ) as string[];
-
-console.log(images);
 
 const Gallery: React.FC = () => {
   return (
@@ -19,6 +17,7 @@ const Gallery: React.FC = () => {
       <div className="grid grid-cols-2 gap-4 py-6 sm:grid-cols-2 md:mx-auto md:w-[600px] md:grid-cols-3">
         {images.map((image, index) => (
           <img
+            key={index + 1}
             src={image}
             alt={`Gallery item ${index + 1}`}
             className="h-48 w-full border border-black object-cover"
