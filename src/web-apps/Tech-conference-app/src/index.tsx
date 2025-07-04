f0336425c3c69d6031238503033c040434d320c8
maxcodingtx <maxcodingtx@gmail.com>
2025-07-03 21:32:43 -0500

    move index.tsx file

---
+++ src/web-apps/Tech-conference-app/src/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Hero from "./components/Hero";
+import "../index.css";
+const Index: React.FC = () => {
+  return (
+    <>
+      <Hero></Hero>
+    </>
+  );
+};
+
+export default Index;
