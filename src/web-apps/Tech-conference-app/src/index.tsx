175c8a2013d3244e4dd94c88dea72facbd7d9775
maxcodingtx <maxcodingtx@gmail.com>
2025-07-05 23:37:10 -0500

    Add Sponsors component to index.tsx

--- src/web-apps/Tech-conference-app/src/index.tsx
+++ src/web-apps/Tech-conference-app/src/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./components/Hero";
+import Sponsors from "./components/Sponsors";
 import "../index.css";
 const Index: React.FC = () => {
   return (
     <>
       <Hero></Hero>
+      <Sponsors></Sponsors>
     </>
   );
 };
