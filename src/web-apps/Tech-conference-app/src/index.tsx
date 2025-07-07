ed2b2cef0311eb7ed185412816e873ba32abe44c
maxcodingtx <maxcodingtx@gmail.com>
2025-07-07 18:11:39 -0500

    import Information component.

--- src/web-apps/Tech-conference-app/src/index.tsx
+++ src/web-apps/Tech-conference-app/src/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./components/Hero";
 import Sponsors from "./components/Sponsors";
+import Information from "./components/Information";
 import "../index.css";
 const Index: React.FC = () => {
   return (
     <>
       <Hero></Hero>
       <Sponsors></Sponsors>
+      <Information></Information>
     </>
   );
 };
