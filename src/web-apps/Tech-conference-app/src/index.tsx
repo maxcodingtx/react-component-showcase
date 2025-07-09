dd627b713731d3d102df2cf619a1cdc015b5fbf7
maxcodingtx <maxcodingtx@gmail.com>
2025-07-09 15:32:21 -0500

    Wrap both Speakers and Footer components in <section> tag. Import image used in background for both components.

--- src/web-apps/Tech-conference-app/src/index.tsx
+++ src/web-apps/Tech-conference-app/src/index.tsx
@@ -3,6 +3,8 @@ import Hero from "./components/Hero";
 import Sponsors from "./components/Sponsors";
 import Information from "./components/Information";
 import Speakers from "./components/Speakers";
+import Footer from "./components/Footer";
+import genericBackground from "./images/generic-bg.png";
 import "../index.css";
 const Index: React.FC = () => {
   return (
@@ -10,7 +12,13 @@ const Index: React.FC = () => {
       <Hero></Hero>
       <Sponsors></Sponsors>
       <Information></Information>
-      <Speakers></Speakers>
+      <section
+        className="bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: `url(${genericBackground})` }}
+      >
+        <Speakers></Speakers>
+        <Footer></Footer>
+      </section>
     </>
   );
 };
