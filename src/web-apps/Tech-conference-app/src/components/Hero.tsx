6dd2bb645b3afaaf7bd8a2e3ce1b94ecb9f181bd
maxcodingtx <maxcodingtx@gmail.com>
2025-07-11 12:25:13 -0500

    apply styling for larger screen sizes.

--- src/web-apps/Tech-conference-app/src/components/Hero.tsx
+++ src/web-apps/Tech-conference-app/src/components/Hero.tsx
@@ -8,15 +8,18 @@ const registerUser = () => {
 export const Hero: React.FC = () => {
   return (
     <section
-      className="*:font-roboto bg-blue-950 bg-cover bg-center px-3 py-20 *:text-center *:text-white"
+      className="bg-blue-950 bg-cover bg-center px-3 py-20 *:text-center *:font-[roboto] *:text-white"
       style={{ backgroundImage: `url(${heroBackgroundImage})` }}
     >
-      <h1 className="text-center text-2xl font-semibold">TECH-WORLD 2025</h1>
-      <p className="my-3 text-xs">March 7-9, 2025 - San Francisco, CA</p>
-      {/* make it a 'common' component  if used again */}
+      <h1 className="text-center text-2xl font-semibold md:text-4xl">
+        TECH-WORLD 2025
+      </h1>
+      <p className="my-3 text-xs md:text-lg">
+        March 7-9, 2025 - San Francisco, CA
+      </p>
       <button
         type="button"
-        className="mx-auto block cursor-pointer rounded-xl bg-radial from-blue-500 to-indigo-800 px-4 py-1.5 text-xs font-semibold active:translate-y-0.5"
+        className="mx-auto block cursor-pointer rounded-xl bg-radial from-blue-500 to-indigo-800 px-4 py-1.5 text-xs font-semibold active:translate-y-0.5 md:px-8 md:py-2 md:text-lg"
         onClick={registerUser}
       >
         Register Now
