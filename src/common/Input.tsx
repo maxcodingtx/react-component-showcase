46c28399d65ddbd9f95e424117f72bd10ce41ba1
maxcodingtx <maxcodingtx@gmail.com>
2025-07-16 15:51:14 -0500

    refactor input component.

--- src/common/Input.tsx
+++ src/common/Input.tsx
@@ -1,56 +1,31 @@
 import React from "react";
 
-interface InputProps {
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   id?: string;
   label?: string;
-  type: "text" | "email" | "password" | "number" | "date";
-  value?: string;
-  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  placeholder?: string;
-  className?: string;
-  required?: boolean;
-  disabled?: boolean;
-  ref?: React.Ref<HTMLInputElement>;
 }
 
-const Input: React.FC<InputProps> = ({
-  id,
-  label,
-  type = "text",
-  value,
-  onChange,
-  placeholder,
-  className = "",
-  required = false,
-  disabled = false,
-  ref,
-}: InputProps &
-  React.InputHTMLAttributes<HTMLInputElement> & {
-    ref?: React.Ref<HTMLInputElement>;
-  }) => {
-  return (
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ id, label, className = "", required = false, ...props }, ref) => (
     <div className="mx-[10px] my-[8px]">
-      <label
-        htmlFor={id}
-        className="mb-0 block text-sm font-bold text-gray-700"
-      >
-        {label}
-        {required && <span className="ml-1 text-red-500">*</span>}
-      </label>
+      {label && (
+        <label
+          htmlFor={id}
+          className="mb-0 block text-sm font-bold text-gray-700"
+        >
+          {label}
+          {required && <span className="ml-1 text-red-500">*</span>}
+        </label>
+      )}
       <input
-        type={type}
         id={id}
-        name={id}
-        value={value}
-        onChange={onChange}
-        placeholder={placeholder}
-        required={required}
-        disabled={disabled}
         ref={ref}
-        className={`focus:shadow-outline w-full rounded border px-3 py-2 text-gray-700 shadow ${disabled ? "cursor-not-allowed bg-gray-200" : "hover:border-sky-600"} ${className}`}
+        required={required}
+        className={`focus:shadow-outline w-full rounded border px-3 py-2 text-gray-700 shadow ${props.disabled ? "cursor-not-allowed bg-gray-200" : "hover:border-sky-600"} ${className}`}
+        {...props}
       />
     </div>
-  );
-};
+  ),
+);
 
 export default Input;
