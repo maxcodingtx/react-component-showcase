94f64b0bf5c5174815fc06c008ba5d03f13b3ee6
maxcodingtx <maxcodingtx@gmail.com>
2025-07-19 18:36:47 -0500

    create hexToTgb function, extracts RGB values from passed hex color code.

---
+++ src/components/hex-rgb-converter/hexToRgb.ts
@@ -0,0 +1,18 @@
+export function hexToRgb(hex: string): number[] {
+  let cleanHex = hex.startsWith("#") ? hex.slice(1) : hex;
+
+  if (cleanHex.length === 3) {
+    cleanHex = cleanHex
+      .split("")
+      .map((char) => char + char)
+      .join("");
+  }
+
+  // Parse red, green, and blue values
+  const r = parseInt(cleanHex.substring(0, 2), 16);
+  const g = parseInt(cleanHex.substring(2, 4), 16);
+  const b = parseInt(cleanHex.substring(4, 6), 16);
+
+  // Return RGB values
+  return [r, g, b];
+}
