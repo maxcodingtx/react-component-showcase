3c6573ac1eba6ed6730a0a51af3738ede93eb091
maxcodingtx <maxcodingtx@gmail.com>
2025-07-21 19:16:05 -0500

    refactor button to spread rest of props.

--- src/common/Button.tsx
+++ src/common/Button.tsx
@@ -4,11 +4,19 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
-const Button: React.FC<ButtonProps> = ({ children, className = "" }) => {
+const Button: React.FC<ButtonProps> = ({
+  children,
+  className = "",
+  ...props
+}) => {
   const buttonStyles =
     "mx-[10px] my-[8px] cursor-pointer rounded-[6px] border-[4px] border-solid border-sky-300 bg-white px-[12px] py-[9px] font-sans font-[550] text-black shadow-[0px_4px_6px_0px_rgba(0,_0,_0,_0.1)] hover:bg-gray-200 active:scale-95";
 
-  return <button className={`${buttonStyles} ${className}`}>{children}</button>;
+  return (
+    <button className={`${buttonStyles} ${className}`} {...props}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
