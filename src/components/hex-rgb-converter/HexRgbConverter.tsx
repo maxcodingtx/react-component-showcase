1dc47e9b0aaefba6e1f31193392c9d0e10f8897a
maxcodingtx <maxcodingtx@gmail.com>
2025-07-21 19:15:51 -0500

    apply styles to component.

--- src/components/hex-rgb-converter/HexRgbConverter.tsx
+++ src/components/hex-rgb-converter/HexRgbConverter.tsx
@@ -2,22 +2,22 @@ import { useState } from "react";
 import { hexToRgb } from "./hexToRgb";
 import { testHex } from "./testHex";
 import Input from "../../common/Input";
+import Button from "../../common/Button";
 
 export default function HexRgbConverter() {
   const [hexValue, setHexValue] = useState("");
   const [rgbValue, setRgbValue] = useState<number[]>([]);
 
-  const cleanHex = hexValue.startsWith("#") ? hexValue.slice(0) : hexValue;
-
+  const cleanHex = hexValue.replace(/^#/, "");
   return (
     <>
-      <h1>Hex To RGB</h1>
+      <h1 className="text-center text-3xl font-bold">Hex To RGB</h1>
 
-      <div className="">
+      <div className="*:my-5">
         {/* user input */}
-        <div className="">
-          <div className="">
-            <p>Enter Hex Value</p>
+        <div className="flex items-center justify-center">
+          <div>
+            <p className="mx-2.5">Enter Hex Value</p>
             <Input
               value={hexValue}
               onChange={(e) => setHexValue(e.target.value)}
@@ -25,47 +25,58 @@ export default function HexRgbConverter() {
           </div>
           {/* hex value color preview */}
           <div
-            className={
+            className="h-30 w-30 border-2 border-black text-center"
+            // style tag due to dynamic background color
+            style={
               testHex(hexValue) &&
               (cleanHex.length === 3 || cleanHex.length === 6)
-                ? `h-30 w-30 bg-[#${cleanHex}] border-2 border-black`
-                : `h-30 w-30 border-2 border-black bg-gray-200`
+                ? { backgroundColor: `#${cleanHex}` }
+                : { backgroundColor: "#e5e7eb" }
             }
-          ></div>
+          >
+            {testHex(hexValue) &&
+            (cleanHex.length === 3 || cleanHex.length === 6)
+              ? `#${cleanHex}`
+              : "Invalid Hex"}
+          </div>
         </div>
 
-        <div className="">
-          <button
+        <div className="flex justify-center">
+          <Button
             type="button"
+            className="w-1/8"
             onClick={() => {
               const rgbValues = hexToRgb(hexValue);
               setRgbValue(rgbValues);
             }}
           >
             Convert
-          </button>
-          <button
+          </Button>
+          <Button
             type="button"
+            className="w-1/8"
             onClick={() => {
               setHexValue("");
               setRgbValue([]);
             }}
           >
             Reset
-          </button>
+          </Button>
         </div>
       </div>
 
       {/* converted values */}
 
-      <div className="">
-        <h2>Converted RGB Values</h2>
+      <div className="my-5 flex flex-col items-center">
+        <h2 className="text-center text-2xl font-bold">Converted RGB Values</h2>
         {rgbValue.length > 0 ? (
           <>
             <p>
               R: {rgbValue[0]}, G: {rgbValue[1]}, B: {rgbValue[2]}
             </p>
-            <p>CSS - RGB({`${rgbValue.join(", ")}`})</p>
+            <p className="font-semibold select-all">
+              RGB({`${rgbValue.join(", ")}`})
+            </p>
           </>
         ) : (
           <p>No RGB values to display</p>
