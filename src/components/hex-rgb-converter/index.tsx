d33713b5b299f837867c1bd964d47f73dbcbd966
maxcodingtx <maxcodingtx@gmail.com>
2025-07-22 19:23:36 -0500

    create index file hex-rgb-converter

---
+++ src/components/hex-rgb-converter/index.tsx
@@ -0,0 +1,9 @@
+import HexRgbConverter from "./HexRgbConverter";
+
+export default function HexToRgbContainer() {
+  return (
+    <div className="py-5">
+      <HexRgbConverter></HexRgbConverter>
+    </div>
+  );
+}
