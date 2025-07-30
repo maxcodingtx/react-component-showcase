e4418a839c3693052a635397ea856098688ec628
maxcodingtx <maxcodingtx@gmail.com>
2025-07-30 13:54:42 -0500

    Implement BarLoader spinner. wrap fetch function in setTimeout for spinner

--- src/components/blog-post-fetch/BlogPostContainer.tsx
+++ src/components/blog-post-fetch/BlogPostContainer.tsx
@@ -1,27 +1,47 @@
 import { useState } from "react";
 import Button from "../../common/Button";
 import { type Blog, fetchPost } from "./fetchBlogPost";
+import { BarLoader } from "react-spinners";
 
 export default function BlogPostContainer() {
   const [post, setPost] = useState<Blog | null>(null);
   const randomId = Math.floor(Math.random() * 3) + 1;
+  const [loading, setLoading] = useState(false);
 
   return (
     <div className="mx-auto mt-5 block max-w-2xl border-5 border-sky-300 p-5">
       <h1 className="mb-4 text-center text-4xl">
         Random Blog Post <br /> Generator
       </h1>
-      <Button
-        className="mx-auto block"
-        type="button"
-        onClick={() => {
-          fetchPost(randomId)
-            .then((data) => setPost(data))
-            .catch((error) => console.error("Failed to fetch post:", error));
-        }}
-      >
-        Get Random Post
-      </Button>
+      <div className="">
+        <Button
+          className="mx-auto block"
+          type="button"
+          onClick={() => {
+            setPost(null);
+            setLoading(true);
+
+            setTimeout(() => {
+              fetchPost(randomId)
+                .then((data) => setPost(data))
+                .then(() => setLoading(false))
+                .catch((error) =>
+                  console.error("Failed to fetch post:", error),
+                );
+            }, 1000);
+          }}
+        >
+          Get Random Post
+        </Button>
+
+        {loading && (
+          <div className="mt-5 flex justify-center">
+            <BarLoader
+              cssOverride={{ backgroundColor: "oklch(82.8% 0.111 230.318)" }}
+            />
+          </div>
+        )}
+      </div>
 
       <article className="px-20 text-center">
         <h1 className="mb-4 text-3xl text-black capitalize">{post?.title}</h1>
