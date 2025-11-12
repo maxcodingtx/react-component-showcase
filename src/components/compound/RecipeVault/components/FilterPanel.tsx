f50ed526c85fb13c0bcba450bb369fcd6577821c
maxcodingtx <maxcodingtx@gmail.com>
2025-11-12 13:02:09 -0600

    create FilterPanel component.

---
+++ src/components/compound/RecipeVault/components/FilterPanel.tsx
@@ -0,0 +1,33 @@
+import { type Recipe } from "../types/recipe";
+
+type AcceptedDifficulties =
+  | Pick<Recipe, "difficulty">["difficulty"]
+  | "All Difficulties";
+
+interface FilterPanelProps {
+  difficulties: AcceptedDifficulties[];
+  selectedDifficulty: AcceptedDifficulties;
+  onFilterChange: (difficulty: AcceptedDifficulties) => void;
+}
+
+export const FilterPanel = ({
+  difficulties,
+  selectedDifficulty,
+  onFilterChange,
+}: FilterPanelProps) => {
+  return (
+    <select
+      className="select select-primary"
+      value={selectedDifficulty}
+      onChange={(e) => {
+        onFilterChange(e.target.value as AcceptedDifficulties);
+      }}
+    >
+      {difficulties.map((difficulty) => (
+        <option key={difficulty} value={difficulty}>
+          {difficulty}
+        </option>
+      ))}
+    </select>
+  );
+};
