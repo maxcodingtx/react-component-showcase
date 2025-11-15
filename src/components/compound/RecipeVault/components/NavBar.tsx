5f3fbbceb893154b8da1dc1880818e690b9fd3b6
maxcodingtx <maxcodingtx@gmail.com>
2025-11-14 19:51:19 -0600

    create NavBar component.

---
+++ src/components/compound/RecipeVault/components/NavBar.tsx
@@ -0,0 +1,23 @@
+import { NavLink } from "react-router";
+
+type navBarProps = {
+  favoritesCount?: number;
+};
+
+export const NavBar = ({ favoritesCount }: navBarProps) => {
+  return (
+    <nav className="flex items-center justify-between bg-gray-800 p-4 text-white">
+      <NavLink to="/" className="text-xl font-bold">
+        Recipe Vault
+      </NavLink>
+      <div>
+        <NavLink to="/" className="mr-4">
+          Home
+        </NavLink>
+        <NavLink to="/favorites" className="mr-4">
+          Favorites : {favoritesCount}
+        </NavLink>
+      </div>
+    </nav>
+  );
+};
