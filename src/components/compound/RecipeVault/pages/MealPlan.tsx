dd74b0179c1ece12f29a3808b10d502f6112dc7a
maxcodingtx <maxcodingtx@gmail.com>
2025-11-19 23:36:56 -0600

    create MealPlan page component.

---
+++ src/components/compound/RecipeVault/pages/MealPlan.tsx
@@ -0,0 +1,41 @@
+import { type Recipe } from "../types/recipe";
+import { MealPlanCalendar } from "../components/MealPlanCalendar";
+import { MealPlanProvider } from "../hooks/useMealPlan";
+import { useContext } from "react";
+import { type MealPlanEntry } from "../components/MealPlanCalendar";
+import { RecipesProvider } from "../hooks/useRecipes";
+
+export const MealPlan = () => {
+  const recipesContext = useContext(RecipesProvider);
+  const recipes = recipesContext?.recipes || [];
+  const mealPlanContext = useContext(MealPlanProvider);
+  const mealPlan = mealPlanContext?.mealPlan || [];
+
+  const handleAddMeal = (date: string, mealType: string, recipe: Recipe) => {
+    const mealPlanEntry: MealPlanEntry = {
+      id: recipe.id.toString(),
+      date,
+      mealType: mealType as MealPlanEntry["mealType"],
+      recipe,
+    };
+    mealPlanContext?.addToMealPlan(mealPlanEntry);
+  };
+  const handleRemoveMeal = (recipeId: string) => {
+    mealPlanContext?.removeFromMealPlan(recipeId);
+  };
+
+  return (
+    <div>
+      <h1 className="mb-4 text-2xl font-bold">Meal Plan</h1>
+
+      <MealPlanCalendar
+        recipes={recipes}
+        mealPlan={mealPlan}
+        onAddMeal={(date, mealType, recipe) => {
+          handleAddMeal(date, mealType, recipe);
+        }}
+        onRemoveMeal={(recipeId: string) => handleRemoveMeal(recipeId)}
+      />
+    </div>
+  );
+};
