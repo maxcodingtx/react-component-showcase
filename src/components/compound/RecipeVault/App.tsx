4735343c0c7d306eaaa23297823ef7b0cafdd165
maxcodingtx <maxcodingtx@gmail.com>
2025-11-20 14:11:54 -0600

    create RecipeVaultApp component for recipe vault and searching.

---
+++ src/components/compound/RecipeVault/App.tsx
@@ -0,0 +1,67 @@
+import {
+  SearchBar,
+  RecipeCard,
+  FavoriteButton,
+  NavBar,
+} from "./components/barrel";
+import { useState, useContext } from "react";
+import { RecipesProvider } from "./hooks/useRecipes";
+import { FavoritesProvider } from "./hooks/useFavorites";
+
+export const RecipeVaultApp = () => {
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
+  const recipesContext = useContext(RecipesProvider);
+  const recipes = recipesContext?.recipes || [];
+
+  const favoritesContext = useContext(FavoritesProvider);
+  const favoriteRecipesIds = favoritesContext?.favorites || [];
+
+  const filteredRecipes = recipes.filter(
+    (recipe) =>
+      recipe.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      recipe.description.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      recipe.category.toLowerCase().includes(searchQuery.toLowerCase()),
+  );
+
+  return (
+    <div>
+      <NavBar favoritesCount={favoriteRecipesIds.length} />
+      <div className="mx-auto max-w-7xl">
+        <h1 className="my-4 text-2xl font-bold">Recipe Vault</h1>
+        <SearchBar
+          query={searchQuery}
+          onChange={(newQuery) => {
+            setSearchQuery(newQuery);
+          }}
+        />
+        <p>
+          {filteredRecipes.length > 0
+            ? filteredRecipes.length > 1
+              ? `${filteredRecipes.length} recipes found.`
+              : "1 recipe found."
+            : "No recipes found."}
+        </p>
+        <div className="grid auto-rows-fr grid-cols-1 gap-4 *:px-4 *:py-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {filteredRecipes.map((recipe) => (
+            <div
+              key={recipe.id}
+              className="bg-base-300 hover:bg-accent-content rounded-lg shadow-lg hover:shadow-2xl"
+            >
+              <RecipeCard recipe={recipe} />
+              <div className="card-actions justify-end p-4">
+                <FavoriteButton
+                  isFavorite={favoriteRecipesIds.includes(recipe.id)}
+                  onToggle={() => {
+                    favoritesContext?.toggleFavorite(recipe.id);
+                    console.log("Toggled favorite for recipe ID:", recipe.id);
+                  }}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
