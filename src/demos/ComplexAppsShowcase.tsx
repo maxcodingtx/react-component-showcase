5f93afd24c4a60b0a3cc77094445b452f6f342cd
maxcodingtx <maxcodingtx@gmail.com>
2025-12-13 01:28:33 -0600

    feat: add ComplexAppsShowcase component to display MovieExplorer and RecipeVault

---
+++ src/demos/ComplexAppsShowcase.tsx
@@ -0,0 +1,11 @@
+import MovieExplorerMain from "../web-apps/MovieExplorer/main";
+import RecipeVaultMain from "../web-apps/RecipeVault/main";
+
+export default function ComplexAppsShowcase() {
+  return (
+    <div>
+      <MovieExplorerMain />
+      <RecipeVaultMain />
+    </div>
+  );
+}
